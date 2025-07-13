c04ffe74182ec9b04f1e342b3869b8df5f825af5
Maylvino Christo Heman <aldenpetra33@gmail.com>
2025-07-13 23:22:37 +0700

    Fix: add placeholder component for admin/cart page

---
+++ src/pages/admin/cart.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AdminCartPage = () => {
+  return (
+    <div>
+      <h1>Admin Cart Page (Coming Soon)</h1>
+    </div>
+  );
+};
+
+export default AdminCartPage;
