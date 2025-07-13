68949bdcc5b30f8d808a0e445127f3399ff817d6
Maylvino Christo Heman <aldenpetra33@gmail.com>
2025-07-13 23:19:59 +0700

    Fix: convert next.config.ts to valid next.config.mjs for Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  //swcMinify: true,
+  // swcMinify: true, // aktifkan jika perlu
   images: {
     domains: ['mockapi.io', 'via.placeholder.com'],
   },
